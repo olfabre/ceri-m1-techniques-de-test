0d4aeebc4c5dbd7e565f5e749f1a752564288566
olfabre <olfabre@gmail.com>
2024-12-11 14:12:42 +0100

    111224H

--- src/main/java/fr/univavignon/pokedex/api/IPokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokemonMetadataProvider.java
@@ -8,11 +8,11 @@ public interface IPokemonMetadataProvider {
 
     /**
      * Récupère et renvoie les métadonnées du pokémon.
-     * dénoté par le <tt>index</tt> donné.
+     * dénoté par le index donné.
      *
      * @param index Index du pokémon pour lequel des métadonnées doivent être récupérées.
      * @return Métadonnées du pokémon.
-     * @throws PokedexException Si le <tt>index</tt> donné n'est pas valide.
+     * @throws PokedexException Si le index donné n'est pas valide.
      */
     PokemonMetadata getPokemonMetadata(int index) throws PokedexException;
 
